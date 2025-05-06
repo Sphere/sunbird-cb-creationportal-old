107c00a451986358099315b4dfc55bbe3c4ab1b2
likhithgowda <likhith.thammegowda@tarento.com>
2025-05-06 16:00:41 +0530

    Sonar integration

--- project/ws/author/src/lib/modules/shared/pipes/mime-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MimeTypePipe } from './mime-type.pipe'
-
-describe('MimeTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MimeTypePipe()
-    expect(pipe).toBeTruthy()
-  })
-})
